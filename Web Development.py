c9e5667ee459c6579a20d93e8128b73a946c9649
Opoti Alvin Waswa <150916755+Alvin-Opoti-Waswa@users.noreply.github.com>
2024-05-22 10:56:05 +0200

    Create Web Development.py

---
+++ Web Development.py
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Heading</h1>
+<h2>This is heading 2</h2>
+<h3>This is heading 3</h3>
+<p>This is a paragraph.</p>
+<p>This is another paragraph.</p>
+
+<h2>HTML Links</h2>
+<p>HTML links are defined with the a tag:</p>
+
+<a href="https://chmurka.wseiz.pl/index.php/s/DSkTIyJODIu7OJi">School Website</a>
+
+<h2>HTML Images</h2>
+<p>HTML images are defined with the img tag:</p>
+
+<img src="w3schools.jpg" alt="W3Schools.com" width="104" height="142">
+
+</body>
+</html>
